20f6e014f3133a83499f26afaefedf3cc267a389
mateoim <mateoim@me.com>
2022-03-10 13:19:55 +0100

    Update main

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-rest-api/controllers"
 	"go-rest-api/models"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 	models.ConnectToDatabase()
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "hello world"})
-	})
+	r.GET("/organizations", controllers.GetOrganizations)
+	r.POST("/organizations", controllers.CreateOrganization)
+	r.GET("/organizations/:id", controllers.GetOrganization)
+	r.DELETE("/organizations/:id", controllers.DeleteOrganization)
+	r.PATCH("/organizations/:id", controllers.UpdateOrganization)
 
 	r.Run()
 }
