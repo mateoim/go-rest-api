a25f73bd74378bb9941ad95cd19a7e5ff6b4b82c
mateoim <mateoim@me.com>
2022-03-13 17:19:30 +0100

    Add Invitation struct

---
+++ models/invitation.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"encoding/json"
+)
+
+type Invitation struct {
+	ID        uint             `json:"id" gorm:"primarykey"`
+	MeetingID uint             `json:"meeting" binding:"required"`
+	UserID    uint             `json:"user" binding:"required"`
+	Status    InvitationStatus `json:"status"`
+}
+
+type InvitationStatus int
+
+const (
+	Pending InvitationStatus = iota + 1
+	Accepted
+	Rejected
+)
+
+func (status InvitationStatus) MarshalJSON() ([]byte, error) {
+	return json.Marshal(status.String())
+}
+
+func (status InvitationStatus) String() string {
+	return [...]string{"Pending", "Accepted", "Rejected"}[status-1]
+}
