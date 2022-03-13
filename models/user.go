1085447e6bf8ec529ed1362d84f2c324684249a4
mateoim <mateoim@me.com>
2022-03-13 17:13:29 +0100

    Add Events to User struct

--- models/user.go
+++ models/user.go
@@ -4,8 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName      string `json:"first-name" binding:"required"`
-	LastName       string `json:"last-name" binding:"required"`
-	Email          string `json:"email" binding:"required,email"`
-	OrganizationID uint   `json:"organization" binding:"required"`
+	FirstName      string  `json:"first-name" binding:"required"`
+	LastName       string  `json:"last-name" binding:"required"`
+	Email          string  `json:"email" binding:"required,email"`
+	OrganizationID uint    `json:"organization" binding:"required"`
+	Events         []Event `gorm:"many2many:event_users;"`
 }
