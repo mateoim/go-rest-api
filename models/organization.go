63710265c88eaf3903544306c7601bc9851582ce
mateoim <mateoim@me.com>
2022-03-13 23:35:54 +0100

    Remove Users from JSON in Organization model

--- models/organization.go
+++ models/organization.go
@@ -3,5 +3,5 @@ package models
 type Organization struct {
 	ID    uint   `json:"id" gorm:"primarykey"`
 	Name  string `json:"name" binding:"required"`
-	Users []User `json:"users"`
+	Users []User `json:"-"`
 }
