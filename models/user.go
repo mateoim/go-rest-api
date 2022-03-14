2b8424151073ddce59e4c128919c6fb9ca4f8d8f
mateoim <mateoim@me.com>
2022-03-14 10:05:31 +0100

    Remove Events field from User JSON

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	LastName       string       `json:"last-name" binding:"required"`
 	Email          string       `json:"email" binding:"required,email"`
 	OrganizationID uint         `json:"organization" binding:"required"`
-	Events         []Event      `gorm:"many2many:event_users;"`
+	Events         []Event      `json:"-" gorm:"many2many:event_users;"`
 	Meetings       []Meeting    `json:"-" gorm:"many2many:user_meetings"`
 	Invitations    []Invitation `json:"-"`
 }
